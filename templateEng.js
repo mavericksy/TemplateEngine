1734dfa68faad7047118cf8d34887accb69da91a
SumoSudo <shaun-p@mweb.co.za>
2017-07-27 10:55:29 +0200

    Update templateEng.js

--- templateEng.js
+++ templateEng.js
@@ -12,6 +12,31 @@ function escapeText(s){
         case"&":return"&amp";
       }});}
 
+// Tag identifier and bits
+function getBlob(kk){
+    switch(kk){
+      case 'd': case 'div': case 'dv':/**/s='<div';t = '>';e = '</div>';
+        break;
+      case 'i':case 'input':case 'in':/**/s='<input';t = '>';e = '';
+        break;
+      case 'img':case 'image':case 'im':/**/s='<img';t='>';e='';
+        break;
+      case 'f':case 'fm':case 'frm':case 'form':/**/s='<form';t='>';e='</form>';
+        break;
+      case 'tx':case 'tex':case 'ta':/**/s='<textarea';t='>';e='</textarea>';
+        break;
+      case 'a':case 'link':case 'lk':case 'lnk':/**/s='<a';t='>';e='</a>';
+        break;
+      case 'h2':case 'big':case '2':/**/s='<h2>';t='';e='</h2>';
+        break;
+      case 'p':case 'para':/**/s='<p>';t='';e='</p>';
+        break;
+      default:
+        return undefined;
+    }
+    return {start:s,tail:t,end:e}
+}
+
 // Oh recursion you beauty!
 // To my rescue you descend
 // up trees I cannot fathom
@@ -22,85 +47,41 @@ function escapeText(s){
 function recursiveOptions(obj,dd){
   if(obj === null || obj === undefined || obj === false || Object.keys(obj).length === 0) return '';
   var base = '';
-  var start,end,tail;
+  var blob,start,end,tail;
   for (var k in obj){
     var kk = obj[k];
     // t: will assign the building blocks
-    switch(kk.t){
-      case 'd':
-        start='<div';tail = '>';end = '</div>';
-        break;
-      case 'i':
-        start='<input';tail = '>';end = '';
-        break;
-      case 'img':
-        start='<img';tail='>';end='';
-        break;
-      case 'form':
-        start='<form';tail='>';end='</form>';
-        break;
-      case 'tx':
-        start='<textarea';tail='>';end='</textarea>';
-        break;
-      case 'a':
-        start='<a';tail='>';end='</a>';
-        break;
-    }
+    if(!(blob = getBlob(kk.t))) return '';
 
     var inc = function(){
       if(dd) return dd++;
       else return '';
     };
-
     // Map the objects to the tags
-    base += start;
-    var a = $.makeArray(kk);
-    kk = a[0];
+    base += blob.start;
+    
     $.map(kk,function(val,key){
       if(!(key=='con'||key=='n'||key=='t')){
         base += (key=='id') ? ' '+key+'="'+val+inc()+'"' : 
           ' '+key+'="'+val+'"' ;
       }
     });
-    base += tail;
+    
+    base += blob.tail;
     base += (kk.con) ? ''+kk.con+'' : '';
     // Now it gets hairy..
     base += (kk.n) ? recursiveOptions(kk.n,dd) : '';
-    base += end;
+    base += blob.end;
   }/*end for obj*/
   return base;
 }/*end recursiveOptions*/
 
-var TemplateEngine = function(html, options) {
-  if(html === null || !html || (options && typeof options != 'object')) {
+var TemplateEngine = function(html) {
+  if(html === null || !html || html === undefined || typeof html == 'string') {
     return '';
   } else {
-    // can embed code in the template
-    // between <% [code] %> tags.
-    var re = /<%(.+?)%>/g,
-    // only run the code that starts like this..
-    reExp = /(^\s*(r*\W|con\W|var\W|if\W|for\W|else\W|switch\W|case\W|break\W|{|})).*/g, 
-    code = 'var r=[];\n', cursor = 0, match;
-    var add = function(line, js) {
-      // add HTML fragments and code.
-      // if the line matched with <% %>
-      //+then add to function executable
-      code += (js) ? ((line.match(reExp)) ? 
-          line + '\n' : 
-          // else add to array. re and reExp matching fault.
-          'r.push(escapeText(' + line + '));\n') : 
-        ((line != '') ?
-         // if not js code else escape the tag quotes and add to array.
-         'r.push("' + line.replace(/"/g, '\\"') + '");\n' : 
-         '');return add;};// end add
-    while((match = re.exec(html))) {
-      add(html.slice(cursor, match.index))(match[1], true);
-      cursor = re.lastIndex;}
-    add(html.substr(cursor, html.length - cursor));
-    code += 'return r.join("");';
-    // concat the HTML fragment array.
-    // apply the options object as this
-    //+and run all code that was added.
-    return Function(code).apply(options);
+    var _id;
+    if(html._id === true) _id = 1; 
+    return recursiveOptions(html.temp,_id);
   }// end main
 };// end TemplateEngine
